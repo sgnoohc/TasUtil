109b271dcc199d1dc48f293348cb75a5c0b8928a
Philip Chang <sgnoohc@gmail.com>
2017-06-22 23:07:32 -0700

    update

--- tasutil.cc
+++ tasutil.cc
@@ -17,20 +17,36 @@ LorentzVector TasUtil::Calc::LV(TLorentzVector p)
 }
 
 //==================================================================================================
-float TasUtil::Calc::DEta(LorentzVector a, LorentzVector b) { return fabs(a.eta() - b.eta());       }
+float TasUtil::Calc::DEta(LorentzVector a, LorentzVector b) { return DEta(TLV(a), TLV(b)); }
+float TasUtil::Calc::DPhi(LorentzVector a, LorentzVector b) { return DPhi(TLV(a), TLV(b)); }
+float TasUtil::Calc::DR  (LorentzVector a, LorentzVector b) { return DR  (TLV(a), TLV(b)); }
+float TasUtil::Calc::DPt (LorentzVector a, LorentzVector b) { return DPt (TLV(a), TLV(b)); }
+float TasUtil::Calc::Mass(LorentzVector a, LorentzVector b) { return Mass(TLV(a), TLV(b)); }
+float TasUtil::Calc::Pt  (LorentzVector a, LorentzVector b) { return Pt  (TLV(a), TLV(b)); }
+float TasUtil::Calc::MT  (LorentzVector a, float met, float met_phi) { return MT(TLV(a), makeMETTLV(met, met_phi)); }
+float TasUtil::Calc::MT  (LorentzVector a, LorentzVector b) { return MT(TLV(a), TLV(b)); }
 
 //==================================================================================================
-float TasUtil::Calc::DPhi(LorentzVector a, LorentzVector b) { return fabs(TLV(a).DeltaPhi(TLV(b))); }
-
-//==================================================================================================
-float TasUtil::Calc::DR  (LorentzVector a, LorentzVector b) { return fabs(TLV(a).DeltaR  (TLV(b))); }
-
-//==================================================================================================
-float TasUtil::Calc::MT  (LorentzVector a, float met, float met_phi)
+float TasUtil::Calc::DEta(TLorentzVector obj1, TLorentzVector obj2) { return fabs(obj1.Eta() - obj2.Eta()); }
+float TasUtil::Calc::DPhi(TLorentzVector obj1, TLorentzVector obj2) { return fabs(obj1.DeltaPhi(obj2));     }
+float TasUtil::Calc::DR  (TLorentzVector obj1, TLorentzVector obj2) { return fabs(obj1.DeltaR(obj2));       }
+float TasUtil::Calc::DPt (TLorentzVector obj1, TLorentzVector obj2) { return fabs(obj1.Pt()-obj2.Pt());     }
+float TasUtil::Calc::Mass(TLorentzVector obj1, TLorentzVector obj2) { return (obj1 + obj2).M();             }
+float TasUtil::Calc::Pt  (TLorentzVector obj1, TLorentzVector obj2) { return (obj1 + obj2).Pt();            }
+float TasUtil::Calc::MT  (TLorentzVector p4  , float met, float met_phi) { return MT(p4, makeMETTLV(met, met_phi)); }
+float TasUtil::Calc::MT  (TLorentzVector p4  , TLorentzVector metp4)
 {
     /// Compute MT
-    TLorentzVector metp4;
-    metp4.SetPtEtaPhiE(met, 0, met_phi, met);
-    float mt = sqrt(2 * a.Pt() * metp4.Pt() * ( 1 - cos(metp4.DeltaPhi(p4) ) ));
+    float mt = sqrt(2 * p4.Pt() * metp4.Pt() * (1 - cos(metp4.DeltaPhi(p4))));
     return mt;
 }
+
+//==================================================================================================
+LorentzVector  TasUtil::Calc::makeMETLV (float met, float met_phi) { return LV(makeMETTLV(met, met_phi)); }
+TLorentzVector TasUtil::Calc::makeMETTLV(float met, float met_phi);
+{
+    TLorentzVector tlv;
+    tlv.SetPtEtaPhiE(met, 0, met_phi, met);
+    return tlv;
+}
+
--- tasutil.h
+++ tasutil.h
@@ -16,10 +16,28 @@ namespace TasUtil
         LorentzVector  LV (TLorentzVector);
 
         // LorentzVector
-        float DPhi(LorentzVector, LorentzVector);
         float DEta(LorentzVector, LorentzVector);
+        float DPhi(LorentzVector, LorentzVector);
         float DR  (LorentzVector, LorentzVector);
+        float DPt (LorentzVector, LorentzVector);
+        float Mass(LorentzVector, LorentzVector);
+        float Pt  (LorentzVector, LorentzVector);
         float MT  (LorentzVector, float, float);
+        float MT  (LorentzVector, LorentzVector);
+
+        // TLorentzVector
+        float DEta(TLorentzVector, TLorentzVector);
+        float DPhi(TLorentzVector, TLorentzVector);
+        float DR  (TLorentzVector, TLorentzVector);
+        float DPt (TLorentzVector, TLorentzVector);
+        float Mass(TLorentzVector, TLorentzVector);
+        float Pt  (TLorentzVector, TLorentzVector);
+        float MT  (TLorentzVector, float, float);
+        float MT  (TLorentzVector, TLorentzVector);
+
+        // Creating MET LorentzVectors
+        LorentzVector  makeMETLV (float, float);
+        TLorentzVector makeMETTLV(float, float);
     }
 }
 
