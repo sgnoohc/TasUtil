60c24a29b38148c3684db4c2b6d75852c14e02f1
Philip Chang <sgnoohc@gmail.com>
2017-07-16 01:36:23 -0700

    upgrade ROOT for JSON

--- root.sh
+++ root.sh
@@ -7,7 +7,7 @@ if $(type root &> /dev/null); then
 else
     echo "Setting up ROOT"
     export SCRAM_ARCH=slc6_amd64_gcc530   # or whatever scram_arch you need for your desired CMSSW release
-    export CMSSW_VERSION=CMSSW_8_0_20
+    export CMSSW_VERSION=CMSSW_9_2_4
     source /cvmfs/cms.cern.ch/cmsset_default.sh
     cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
     eval `scramv1 runtime -sh`
